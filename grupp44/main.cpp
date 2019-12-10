bc01257de703f6047d192e48d67f00378066c424
Emma Furuholm <emmafuruholm@gmail.com>
2019-12-10 15:32:53 +0100

    SDL igen igen

--- grupp44/main.cpp
+++ grupp44/main.cpp
@@ -1,10 +1,12 @@
 
 #include <iostream>
+#include <SDL.h>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
     // insert code here...
-    cout << "Test\n Erik hej hej";
+    cout << SDL_Init(SDL_INIT_EVERYTHING);
     cout << "Test Emma hej  hej";
 
     return 0;
