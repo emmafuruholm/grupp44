4ee225b8457b325ffb9e15738ffbe82d74bd9e42
Emma Furuholm <emmafuruholm@gmail.com>
2019-12-10 11:18:52 +0100

    Initial Commit

---
+++ grupp44/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grupp44
+//
+//  Created by Emma Furuholm on 2019-12-10.
+//  Copyright © 2019 Emma Furuholm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
