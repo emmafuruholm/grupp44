99847468fcdc1b1823c6336041073188c76ab6ec
Emma Furuholm <emmafuruholm@gmail.com>
2019-12-10 15:02:31 +0100

    gitignore

--- grupp44/main.cpp
+++ grupp44/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Test\n Erik";
+    cout << "Test\n Erik hej";
     cout << "Test Emma hej";
 
     return 0;
